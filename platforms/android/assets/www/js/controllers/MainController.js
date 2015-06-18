c23fc9e16d91af554218b85c536414bcc901a790
jinxk <jinxk@git.com>
2015-06-18 07:04:28 +0530

    Updated the Main Controller
    Alert when device ready is working while the contacts display still
    isn't.

--- platforms/android/assets/www/js/controllers/MainController.js
+++ platforms/android/assets/www/js/controllers/MainController.js
@@ -1,22 +1,31 @@
 app.controller('MainController', ['$scope', function($scope){
+
+
     document.addEventListener("deviceready", onDeviceReady, false);
+
     //$scope.contacts=[];
     function onDeviceReady() {
+
         var options      = new ContactFindOptions();
-        options.filter   = "";
+        options.filter = "z";
         options.multiple = true;
         var fields = ["displayName", "name", "phoneNumbers"];
         navigator.contacts.find(fields, onSuccess, onError, options);
+
+        function onSuccess(contacts) {
+            //TODO : This doesn't work yet - the alert is working, but the contacts are not displayed. Investigate.
+            $scope.allContacts=contacts;
+            alert("Mission Successful "+ $scope.allContacts[0].displayName);
+
+        };
+
+        function onError(contactError) {
+            alert('onError!');
+        };
+
     };
 
-    function onSuccess(contacts) {
-        $scope.contacts=["ATM","TTM","PPM"];
-        alert("Mission Successful");
-    };
 
-    function onError(contactError) {
-        alert('onError!');
-    };
 
 
 }
